3c1ca448420c3aa26612e68c195f42932301d1ec
Kashfi Fahim <Kashfi.Fahim@gmail.com>
2024-09-05 11:48:08 -0400

    compile front.c

--- programs/front.c
+++ programs/front.c
@@ -11,7 +11,7 @@ char nextChar;
 int lexLen;
 int token;
 int nextToken;
-FILE *in_fp, *fopen();
+FILE *in_fp;
 
 /* Function declarations */
 void addChar();
@@ -37,7 +37,7 @@ int lex();
 
 /*********/
 /* main driver */
-main() {
+int main() {
 
     /* Open the input data file and process its contents */
     if ((in_fp = fopen("front.in", "r")) == NULL)
@@ -51,3 +51,124 @@ main() {
 }
 
 /******** */
+/* lookup - a function to lookup operators and parentheses and return the token */
+int lookup(char ch) {
+    switch(ch) {
+        case '(':
+        addChar();
+        nextToken = LEFT_PAREN;
+        break;
+    
+        case ')':
+            addChar();
+            nextToken = RIGHT_PAREN;
+            break;
+
+        case '+':
+            addChar();
+            nextToken = ADD_OP;
+            break;
+        
+        case '-':
+            addChar();
+            nextToken = SUB_OP;
+            break;
+        
+        case '*':
+            addChar();
+            nextToken = MULT_OP;
+            break;
+        
+        case '/':
+            addChar();
+            nextToken = DIV_OP;
+            break;
+        
+        default:
+            addChar();
+            nextToken = EOF;
+            break;
+
+    }
+    return nextToken;
+}
+
+
+/***** */
+/* addChar - a function to add nextChar to lexeme */
+void addChar() {
+    if (lexLen <= 98) {
+        lexeme[lexLen++] = nextChar;
+        lexeme[lexLen] = 0;
+    }
+    else 
+        printf("Error - lexeme is too long\n");
+}
+
+/**** */
+/* getChar -- a function to get the next character of input and determine its character class */
+void getChar() {
+    if ((nextChar = getc(in_fp)) != EOF) {
+        if (isalpha(nextChar))
+            charClass = LETTER;
+        else if (isdigit(nextChar))
+            charClass = DIGIT;
+            else charClass = UNKNOWN;
+    }
+    else 
+        charClass = EOF;
+}
+
+/******* */
+/* getNonBlank -  a function to call getChar until it returns a non-whitespace character */
+void getNonBlank() {
+    while (isspace(nextChar))
+        getChar();
+}
+
+/**** */
+/* Lex - a simple lexical analyzer for arithmetic expressions */
+int lex() {
+    lexLen = 0;
+    getNonBlank();
+    switch (charClass) {
+        /* Parse identifiers */
+        case LETTER:
+            addChar();
+            getChar();
+            while (charClass == LETTER || charClass == DIGIT) {
+                addChar();
+                getChar();
+            }
+        nextToken = IDENT;
+        break;
+
+        /* Parse integer literals */
+        case DIGIT:
+            addChar();
+            getChar();
+            while(charClass == DIGIT) {
+                addChar();
+                getChar();
+            }
+            nextToken = INT_LIT;
+            break;
+
+        /* Parentheses and operators */
+        case UNKNOWN:
+            lookup(nextChar);
+            getChar();
+            break;
+
+        /* EOF */
+        case EOF:
+            nextToken = EOF;
+            lexeme[0] = 'E';
+            lexeme[1] = 'O';
+            lexeme[2] = 'F';
+            lexeme[3] = 0;
+            break;
+    }
+    printf("Next token is: %d, Next lexeme is %s\n", nextToken, lexeme);
+    return nextToken;
+}
